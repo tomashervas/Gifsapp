f0c06cddf1e18b3aa382687576f7b4742fa42820
Jose Tomás Hervás <josetomashervas@gmail.com>
2021-07-13 16:45:35 +0200

    generado build

--- src/components/GifItem.js
+++ src/components/GifItem.js
@@ -3,28 +3,36 @@ import React, { useState, useEffect } from 'react'
 export const GifItem = ({cat}) => {
 
     const [imgs, setImg] = useState([]);
-    
+    let offset = Math.floor(Math.random()*1000);
+
     useEffect(()=>{
         getGifs(cat);
     }, [cat]);
 
     const getGifs = async (cat)=>{
-        const offset = Math.floor(Math.random()*1000);
+        
         console.log(offset);
         const url = `https://api.giphy.com/v1/gifs/search?q=${encodeURI(cat)}&limit=8&offset=${offset}&api_key=gRkD5kUUP5gG1YZtyn4jnGoBb25aVn0L`;
         const resp = await fetch( url );
         const {data} = await resp.json();
-        
-        const gifs = data.map(gif=>{
-            return {
-                id:gif.id,
-                titulo: gif.title,
-                url: gif.images.fixed_width.url
-            }
-        })
-        console.log(data)
-        console.log(gifs);
-        setImg(gifs);
+        if(data.length > 0) {
+            const gifs = data.map(gif=>{
+                return {
+                    id:gif.id,
+                    titulo: gif.title,
+                    url: gif.images.fixed_width.url
+                }
+            })
+
+            console.log(data)
+            console.log(gifs);
+            setImg(gifs);
+
+        } else {
+            if(offset===0) return;
+            offset = 0;
+            getGifs(cat)
+        }
     }
 
     return (
@@ -33,7 +41,7 @@ export const GifItem = ({cat}) => {
             <ul className="listaGifs">
                 {imgs.map(img=> {
                     return (
-                        <div key={img.id} className="card">
+                        <div key={img.id} className="card animate__animated animate__fadeIn">
                             <li>
                                 <p>{img.titulo}</p>
                                 <div className="imagen">
